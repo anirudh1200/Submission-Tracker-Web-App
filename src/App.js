26e55abc8dc5e61f9abeae2b7805be2a190671fa
Anirudh Bhat <anirudh.b.123@gmail.com>
2018-11-10 12:29:21 +0530

    Initial commit. npm start

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <h2> This will be the main page </h2>
+      </div>
+    );
+  }
+}
+
+export default App;
