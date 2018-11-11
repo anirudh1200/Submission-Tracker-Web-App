0984809c8f801dae86d1eaa444c6ed2f4b353c50
Anirudh Bhat <anirudh.b.123@gmail.com>
2018-11-11 17:07:05 +0530

    all sidenav links are now working

--- src/components/layout/SignedInLinks.js
+++ src/components/layout/SignedInLinks.js
@@ -4,7 +4,7 @@ import { NavLink } from 'react-router-dom';
 const SignedInLinks = props => {
     return(
         <ul className="bottom">
-            <li onClick={props.closeit}><NavLink to='/'>Add New Submission</NavLink></li>
+            <li onClick={props.closeit}><NavLink to='/create'>Add New Submission</NavLink></li>
             <li onClick={props.closeit}><NavLink to='/'>SignOut</NavLink></li>
         </ul>
     )
--- src/components/layout/SignedOutLinks.js
+++ src/components/layout/SignedOutLinks.js
@@ -4,8 +4,8 @@ import { NavLink } from 'react-router-dom'
 const SignedOutLinks = props => {
     return(
         <ul className="bottom">
-            <li onClick={props.closeit}><NavLink to='/'>SignIn</NavLink></li>
-            <li onClick={props.closeit}><NavLink to='/'>SignUp</NavLink></li>
+            <li onClick={props.closeit}><NavLink to='/signin'>Login</NavLink></li>
+            <li onClick={props.closeit}><NavLink to='/signup'>SignUp</NavLink></li>
         </ul>
     )
 }
